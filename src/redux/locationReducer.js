8910d9aa148bbe9f975f18244a64b39e8a4cf76d
OhJeongHwan1 <wjdghks0316@naver.com>
2024-09-17 01:12:59 +0900

    feat: ThirdSection 작성 - 지도 영역 작성

--- src/redux/locationReducer.js
+++ src/redux/locationReducer.js
@@ -1,5 +1,33 @@
 const initialState = {
-  locationList: [],
+  locationList: [
+    {
+      name: "루이비똥 메종 서울",
+      phone: "010-1234-5678",
+      address: "서울특별시 건대입구",
+      latitude: "37.48322075935205",
+      longitude: "126.91562063536725",
+      image:
+        "https://kr.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton--StFi_Louis_Vuitton_DAEJEON_GALLERIA_933_DI3.jpgwid%7BIMG_WIDTH%7D&hei%7BIMG_HEIGHT%7D?wid=490",
+    },
+    {
+      name: "루이비똥 디지털",
+      phone: "010-2222-3333",
+      address: "서울특별시 가산 디지털단지 33434123-1233131241241",
+      latitude: "37.47594146281152",
+      longitude: "126.89933364425818",
+      image:
+        "https://kr.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton--StFi_Louis_Vuitton_SEOUL_SHINSEGAE_KANGNAM_781_2_DI3.jpgwid%7BIMG_WIDTH%7D&hei%7BIMG_HEIGHT%7D?wid=490",
+    },
+    {
+      name: "루이비똥 메종 가산",
+      phone: "010-4444-5555",
+      address: "서울특별시 가산동 가산구 가산호 가산가산",
+      latitude: "37.465454759280554",
+      longitude: "126.90439426631131",
+      image:
+        "https://kr.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton--StFi_Louis_Vuitton_Jinghua_DI3.jpgwid%7BIMG_WIDTH%7D&hei%7BIMG_HEIGHT%7D?wid=490",
+    },
+  ],
 };
 
 const locationReducer = (state = initialState, action) => {
