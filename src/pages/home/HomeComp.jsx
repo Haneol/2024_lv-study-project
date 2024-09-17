57f3574a9ecd422da36853c85f915bff5ba067ac
jsj7878 <sejin8603@naver.com>
2024-09-17 23:29:56 +0900

    delete cart modal in homeComp

--- src/pages/home/HomeComp.jsx
+++ src/pages/home/HomeComp.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import FirstSection from "./FirstSection";
 import SecondSection from "./SecondSection";
 import ThirdSection from "./ThirdSection";
-import Cart from "../../components/cart/Cart";
 
 function HomeComp() {
   return (
     <div>
-      <Cart />
       <FirstSection />
       <SecondSection />
       <ThirdSection />
