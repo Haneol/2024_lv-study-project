30a73bcc3176bdb0e3d01d875586e735d3406e07
eunsol <eunsol037@naver.com>
2024-09-20 15:38:39 +0900

    button 클릭 시 구현

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import data from "../../data";
+import { useDispatch } from "react-redux";
 
 function Detail() {
   const { id } = useParams();
   const product = data[id];
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [showMessage, setShowMessage] = useState(false);
 
   const goBack = () => {
     navigate(-1);
+  };
+
+  const addToCart = () => {
+    dispatch({
+      type: "@cart/cartItemAdd", // cartReducer에서 정의한 액션 타입
+      payload: product, // 상품 정보를 payload로 전달
+    });
+
+    setShowMessage(true); // 메시지 표시
+
+    // 1초 후 메시지 숨기기
+    setTimeout(() => {
+      setShowMessage(false);
+    }, 1000);
   };
 
   return (
@@ -32,7 +50,10 @@ function Detail() {
             <p className="font-bold text-2xl mb-10 mt-8 text-center xl:text-start">
               {product.price}
             </p>
-            <button className="bg-black bg-opacity-30 text-white rounded-full w-full h-16  hover:bg-gray-500">
+            <button
+              onClick={addToCart}
+              className="bg-black bg-opacity-30 text-white rounded-full w-full h-16  hover:bg-gray-500"
+            >
               쇼핑백에 추가
             </button>
             <p className="text-xs text-gray-800 text-center mt-6">
@@ -41,6 +62,11 @@ function Detail() {
           </div>
         </div>
       </div>
+      {showMessage && (
+        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black text-white px-6 py-4 rounded-lg z-50">
+          장바구니에 상품이 담겼습니다 !
+        </div>
+      )}
     </div>
   );
 }
